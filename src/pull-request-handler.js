2a088bbc0d506b5b24959ced846a3e19d4d451cf
Alex Mos <alexander.moskovkin@devexpress.com>
2015-06-18 14:03:03 +0300

    Refactoring

--- src/pull-request-handler.js
+++ src/pull-request-handler.js
@@ -2,27 +2,6 @@ import GitHub from './github';
 import WebHooksListener from './webhooks-listener';
 import GITHUB_MESSAGE_TYPES from './github-message-types';
 
-//returns sha of the tested commit
-async function _onPullRequest (github, prId, prNumber, pullRequestSha, testBranchName) {
-    return github.createBranch(pullRequestSha, testBranchName)
-        .then(function (res) {
-            return github.createPullRequestComment(prNumber, 'tests started');
-        })
-        .then(function () {
-            return new Promise(resolve => resolve());
-        })
-        .catch(function (err) {
-            throw new Error(err);
-        });
-}
-
-async function _onTestsFinished (github, prNumber, testBranchName, status, reportUrl) {
-    return github.createPullRequestComment(prNumber, status + ': ' + reportUrl)
-        .then(function () {
-            github.deleteBranch(testBranchName);
-        });
-}
-
 //API
 export function init (githubUser, githubRepo, githubOauthToken, webhooksListener) {
     var github = new GitHub(githubUser, githubRepo, githubOauthToken);
@@ -37,8 +16,9 @@ export function init (githubUser, githubRepo, githubOauthToken, webhooksListener
         var prId           = prBody.pull_request.id;
         var prNumber       = prBody.number;
         var testBranchName = 'rp-' + prId;
+        var testsStarted   = false;
 
-        _onPullRequest(github, prId, prNumber, pullRequestSha, testBranchName)
+        github.createBranch(pullRequestSha, testBranchName)
             .then(function () {
                 function onStatusMessage (e) {
                     var statusBody = e.body;
@@ -49,12 +29,16 @@ export function init (githubUser, githubRepo, githubOauthToken, webhooksListener
                     if (statusBody.sha !== pullRequestSha)
                         return;
 
-                    if (statusBody.state === 'pending')
+                    if (statusBody.state === 'pending' && !testsStarted) {
+                        testsStarted = true;
+                        github.createPullRequestComment(prNumber, 'tests started: ' + statusBody.target_url);
                         return;
+                    }
 
                     webhooksListener.off(GITHUB_MESSAGE_TYPES.STATUS, onStatusMessage);
 
-                    _onTestsFinished(github, prNumber, testBranchName, statusBody.state, statusBody.target_url);
+                    github.createPullRequestComment(prNumber, 'Tests ' + statusBody.state);
+                    github.deleteBranch(testBranchName);
                 }
 
                 webhooksListener.on(GITHUB_MESSAGE_TYPES.STATUS, onStatusMessage);
